8ce48cb1a31bc04355ac705cbe5e2ea2a907bd11
Mikkel O. Laursen <m@ikkel.dk>
2021-09-14 18:48:09 +0200

    Yesss

--- src/signups/TwentyFiveMan.ts
+++ src/signups/TwentyFiveMan.ts
@@ -1,4 +1,4 @@
-import { Client, Message, PartialMessage } from "discord.js";
+import { Client, Message, MessageEmbed, PartialMessage } from "discord.js";
 import Container, { Service } from "typedi";
 import { RegEx } from "../types/RegEx";
 import { SignupHandler } from "../types/SignupHandler";
@@ -15,9 +15,8 @@ interface Signup {
 
 @Service()
 export class TwentyFiveMan implements SignupHandler {
-    readonly topLeftMostIndex = [6, 0];
+    readonly topLeftMostIndex = [0, 0];
     readonly offsetBetweenSignups = 20;
-    readonly signups: Signup[] = [];
 
     public run(client: Client): void {
         client.on("messageUpdate", (a, b) => this.onEvent(a, b)); // implicit context binding
@@ -31,29 +30,25 @@ export class TwentyFiveMan implements SignupHandler {
         // ignore anything not from Raid-Helper and in raid 1 sign up channel
         if (msg.author.id !== "579155972115660803" || msg.channelId !== "854104235619909712") return;
 
+        // sorted list by dates of all messages in channel by Raid-Helper
+        const messages = (await msg.channel.messages.fetch())
+            .filter((msg) => msg.author.id === "579155972115660803")
+            .map((m) => m)
+            .sort((a, b) => this.compareDates(this.getDateFromMessageAsObject(a), this.getDateFromMessageAsObject(b)));
+
         const sheet = gService.doc.sheetsById[787890111];
 
-        const signup = this.signups.find((ele) => ele.messageId === msg.id);
+        messages.forEach(async (message, i) => {
+            let index = [this.topLeftMostIndex[0], i * this.offsetBetweenSignups + this.topLeftMostIndex[1]] as [
+                number,
+                number
+            ];
 
-        let index;
-
-        if (!signup) {
-            index = [
-                this.topLeftMostIndex[0],
-                this.signups.length * this.offsetBetweenSignups + this.topLeftMostIndex[1],
-            ] as [number, number];
-
-            this.signups.push({
-                messageId: msg.id,
-                index,
-            });
-        } else {
-            index = signup.index;
-        }
-        await this.updateSheet(index, msg, sheet);
+            await this.updateSheet(index, message, sheet);
+            await sheet.saveUpdatedCells();
+        });
 
         Logger.info(`Updated 25-man sheet`);
-        await sheet.saveUpdatedCells();
     }
 
     private async updateSheet(startingIndex: [number, number], msg: Message, sheet: GoogleSpreadsheetWorksheet) {
@@ -62,13 +57,13 @@ export class TwentyFiveMan implements SignupHandler {
         await sheet.loadCells({
             startColumnIndex: startingIndex[0],
             startRowIndex: startingIndex[1],
-            endColumnIndex: startingIndex[0] + 4,
+            endColumnIndex: startingIndex[0] + 4 + 6,
             endRowIndex: startingIndex[1] + 20,
         });
 
         gService.clearCells(startingIndex, [startingIndex[0] + 3, startingIndex[1] + 19], sheet);
 
-        const date = msg.embeds[0].fields[0].value.split("[")[1].split("]")[0];
+        const date = this.getDateFromMessage(msg);
 
         const title = msg.embeds[0].description
             ?.split(":")
@@ -89,13 +84,57 @@ export class TwentyFiveMan implements SignupHandler {
         const rangedPairs = rangedFields.getNamesAndColors();
         const meleePairs = meleeFields.getNamesAndColors();
 
-        gService.makeCellSignupTitle(startingIndex, sheet);
-        gService.makeCellValue([startingIndex[0], startingIndex[1] + 1], date, sheet);
+        gService.makeCellSetupTitle(startingIndex, sheet);
+        gService.makeCellSignupTitle([startingIndex[0] + 6, startingIndex[1]], sheet);
+
+        gService.makeCellValue([startingIndex[0], startingIndex[1] + 1], date as string, sheet);
+        gService.makeCellValue([startingIndex[0] + 6, startingIndex[1] + 1], date as string, sheet);
+
         gService.makeCellValue([startingIndex[0], startingIndex[1] + 2], title, sheet);
+        gService.makeCellValue([startingIndex[0] + 6, startingIndex[1] + 2], title, sheet);
+
         gService.addRoleHeadlines([startingIndex[0], startingIndex[1] + 3], sheet);
-        gService.updateColumnWithColor([startingIndex[0], startingIndex[1] + 5], tankPairs, sheet);
-        gService.updateColumnWithColor([startingIndex[0] + 1, startingIndex[1] + 5], healerPairs, sheet);
-        gService.updateColumnWithColor([startingIndex[0] + 2, startingIndex[1] + 5], rangedPairs, sheet);
-        gService.updateColumnWithColor([startingIndex[0] + 3, startingIndex[1] + 5], meleePairs, sheet);
+        gService.addRoleHeadlines([startingIndex[0] + 6, startingIndex[1] + 3], sheet);
+
+        gService.updateColumnWithColor([startingIndex[0] + 6, startingIndex[1] + 5], tankPairs, sheet);
+        gService.updateColumnWithColor([startingIndex[0] + 7, startingIndex[1] + 5], healerPairs, sheet);
+        gService.updateColumnWithColor([startingIndex[0] + 8, startingIndex[1] + 5], rangedPairs, sheet);
+        gService.updateColumnWithColor([startingIndex[0] + 9, startingIndex[1] + 5], meleePairs, sheet);
+    }
+
+    private getDateFromMessage(msg: Message, asObject?: boolean): string {
+        return msg.embeds[0].fields[0].value.split("[")[1].split("]")[0];
+    }
+
+    private getDateFromMessageAsObject(msg: Message): Date {
+        return new Date(Number(msg.embeds[0].fields[0].value.split(":")[6]) * 1000);
+    }
+
+    /** https://expertcodeblog.wordpress.com/2018/03/12/typescript-how-to-compare-two-dates/
+     * Compares two Date objects and returns e number value that represents
+     * the result:
+     * 0 if the two dates are equal.
+     * 1 if the first date is greater than second.
+     * -1 if the first date is less than second.
+     * @param date1 First date object to compare.
+     * @param date2 Second date object to compare.
+     */
+    private compareDates(date1: Date, date2: Date): number {
+        // With Date object we can compare dates them using the >, <, <= or >=.
+        // The ==, !=, ===, and !== operators require to use date.getTime(),
+        // so we need to create a new instance of Date with 'new Date()'
+        let d1 = new Date(date1);
+        let d2 = new Date(date2);
+
+        // Check if the dates are equal
+        let same = d1.getTime() === d2.getTime();
+        if (same) return 0;
+
+        // Check if the first is greater than second
+        if (d1 > d2) return 1;
+
+        // Check if the first is less than second
+        if (d1 < d2) return -1;
+        else return 1;
     }
 }
